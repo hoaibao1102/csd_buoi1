106c652784399e43d3f529fd5dad3e1847e95e45
hoaibao1102 <hoaibaole.qng@gmail.com>
2024-11-27 00:47:13 +0700

    "themthu2"

---
+++ Hoctap.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Hoctap {
+    public static void main(String[] args) {
+        System.out.println("hello man");
+        int a =  2;
+        int b = 3;
+        int c;
+        c = a+b;
+        System.out.println("tong cua a va b la: "+c);
+    }
+}
--- main.java
+++ main.java
@@ -1,5 +1,12 @@
 package src;
 
 public class main {
-    
+    public static void main(String[] args) {
+        System.out.println("hello man");
+        int a =  2;
+        int b = 3;
+        int c;
+        c = a+b;
+        System.out.println("tong cua a va b la: "+c);
+    }
 }
